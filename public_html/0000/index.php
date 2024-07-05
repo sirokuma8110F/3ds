4102f55ba97f33eab79fba1a52070f8663e25ad5
sirokuma8110F <haruki.t07110@gmail.com>
2024-07-05 10:38:24 +0000

    set

---
+++ public_html/0000/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>build id [0000]</title>
+    <meta name="viewport" content="width=320">
+    <meta name="description" content="tools[id=0000] for 3DS">
+</head>
+<body>
+</body>
+</html>
